57b41d783f6f999ce08cf6f79e1479e72a9a3112
zubairabid <zubairabid1999+github@gmail.com>
2018-02-23 09:36:46 +0530

    done with 2 altogether

---
+++ Lab/HW5/fib_15.s
@@ -0,0 +1,17 @@
+.globl _start;                                  
+_start:
+
+# Put your own code here
+	addi x5, x0, 15
+	addi x6, x0, 0
+	addi x7, x0, 1
+
+	loop:
+		add x28, x6, x7
+		add x6, x0, x7
+		add x7, x0, x28
+	
+		addi x5, x5, -1
+		bne x5, x0, loop
+
+_exit:
